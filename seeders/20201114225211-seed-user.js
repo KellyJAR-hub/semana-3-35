a5f467f63eb9a481a9c094b3c7b0584ceffb4765
danvelandiav <vvmd25@gmail>
2020-12-12 16:26:06 -0500

    backend proyecto

---
+++ seeders/20201114225211-seed-user.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('users', [{
+            name: 'carlos',
+            email: 'ejemplo@gmail.com',
+            password: '$2y$08$yDjNTG86g7SSt0MJRMY90.fp4O3mWUbj0YUkZZEujYiMA4PgTHGQ6',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }]);
+    },
+
+    down: async(queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('users', null, {});
+
+    }
+};
